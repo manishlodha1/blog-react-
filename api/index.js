459b3e19426b5a27caf2a574b3b26a579b38e077
manishlodha1 <manishlodha015@gmail.com>
2024-01-10 18:23:33 +0530

    update

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use("/images",express.static(path.join(__dirname,"/images")))
 
 async function startServer() {
     try {
-      await mongoose.connect(process.env.MONGO_URL, {useUnifiedTopology: true });
+      await mongoose.connect(process.env.MONGO_URL);
       console.log("Connected to MongoDB");
       app.listen(5000, () => {
         console.log("Backend is running");
